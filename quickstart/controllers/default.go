9faa35369c3607902ae7453f48fa61852d8f4d8e
seonyu.hwang <seonyu.hwang@mac-n059.local>
2019-05-30 17:35:43 +0900

    hwang2 codepipeline start

---
+++ quickstart/controllers/default.go
@@ -0,0 +1,20 @@
+package controllers
+
+import (
+"github.com/astaxie/beego"
+"context"
+"github.com/aws/aws-xray-sdk-go/xray"
+_ "github.com/aws/aws-xray-sdk-go/plugins/beanstalk"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	_, seg := xray.BeginSegment(context.Background(), "/")
+	c.Data["Website"] = "beego.me"
+	c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.tpl"
+	seg.Close(nil)
+}
